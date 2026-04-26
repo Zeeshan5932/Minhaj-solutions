4840ee943f2dbb28da5b0f8880653a07aea93e1d
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-04-26 21:13:42 +0500

    new

---
+++ day.py
@@ -0,0 +1,16 @@
+# firstname = input("Enter your name: ")
+# #secondname = input("enter your second name:")
+# print("Hello! how are you ", firstname, secondname)
+
+
+# mood = input("How are you feeling today? ")
+
+# print("Oh, you are feeling", mood)
+
+# ## 10. Simple Guess Number Game
+print("Guess a number between 1 and 5")
+
+guess = input("Enter your guess: ")
+
+print("You guessed:", guess)
+print("Nice try!")
