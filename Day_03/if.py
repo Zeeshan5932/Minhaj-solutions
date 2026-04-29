7436ca023afc35f39d6ded8d2f0e3c2a3a065603
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-04-29 13:30:27 +0500

    new

---
+++ Day_03/if.py
@@ -0,0 +1,23 @@
+# 
+age = int(input("What is your age: "))
+print("your age is:" , age)
+
+#operator
+
+# > , < ,>= ,<= , == , !=
+
+print(age=='18')
+print(age!=18)
+print(age>18)
+print(age<18)
+
+
+if age >= 18:
+    print('your are allow')
+else:
+    print('your are not allow')
+
+
+
+print('The end of the program')
+    
---
+++ Day_03/python.py
@@ -0,0 +1,51 @@
+# budget = int(input('Enter the your budget for laptop: '))
+
+# if budget >= 1000:
+#     print('You can buy a laptop')
+# elif budget >= 500:
+#     print('You can buy a used laptop')
+    
+# else:
+#     print('You cannot buy a laptop')
+    
+# apple = 200
+# budget = 210
+
+# # 210 - 200 = 10
+# if (budget- apple) >= 15:
+#     print('You can buy an apple')
+    
+# elif (budget - apple) < 15:
+#     print('You cannot buy an apple')
+    
+# else:
+#     print('The end of the program')
+
+
+
+# Nested if elif else 
+
+# num = input('Enter a number: ')
+# num = int(num)
+
+# if num > 0:
+#     print('The number is positive')
+
+# else: 
+#     if num < 0:
+#         print('The number is negative')
+#     else:
+#         print('The number is zero')
+        
+# print('The end of the program')
+
+
+num = 0.1
+
+if num > 0:
+    print('The number is positive')
+
+    
+else:
+    print('The end of the program')
+    
