93bf1ecf87a22a7d71d9ca169ce8c1e8ca5276fe
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-04-30 15:43:13 +0500

    new

--- Day-04/python.py
+++ Day-04/python.py
@@ -1,18 +1,51 @@
-# List + input + control structure
+# Taking basic account information
+name = input("Enter account holder name: ")
+balance = float(input("Enter initial balance: "))
 
-marks = [
-    int(input("Enter Python marks: ")),
-    int(input("Enter English marks: ")),
-    int(input("Enter Computer marks: "))
-]
+# Storing in dictionary
+account = {
+    "name": name,
+    "balance": balance
+}
 
-total = sum(marks)
-average = total / 3
+# Account types (fixed)
+account_types = ("Saving", "Current")
 
-if average >= 50:
-    print("Pass")
+print("Available account types:", account_types)
+acc_type = input("Choose account type: ")
+
+# List to store transactions
+transactions = []
+
+# Ask operation
+operation = input("Enter operation (deposit/withdraw): ")
+
+if operation == "deposit":
+    amount = float(input("Enter deposit amount: "))
+    account["balance"] += amount
+    transactions.append(amount)
+    print("Amount deposited successfully")
+
+elif operation == "withdraw":
+    amount = float(input("Enter withdraw amount: "))
+    
+    if amount <= account["balance"]:
+        account["balance"] -= amount
+        transactions.append(amount)
+        print("Amount withdrawn successfully")
+    else:
+        print("Insufficient Balance")
+
 else:
-    print("Fail")
+    print("Invalid operation")
 
-print("Marks List:", marks)
-print("Average:", average)
+# Set for unique transactions
+unique_transactions = set(transactions)
+
+# Final output
+print("\n--- Bank Summary ---")
+print("Name:", account["name"])
+print("Account Type:", acc_type)
+print("Final Balance:", account["balance"])
+print("All Transactions:", transactions)
+print("Unique Transactions:", unique_transactions)
