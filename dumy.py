e2717657a2127020e1bef937f862b3b3a961d3b3
Zeeshan5932 <zeeshanoffical01@gmail.com>
2026-05-07 16:05:18 +0500

    new

--- dumy.py
+++
@@ -1,6 +0,0 @@
-total_items = int(input("How many items do you want to add? "))
-
-for i in range(total_items):
-    product_name = input("Enter product name: ")
-    price = float(input("Enter price: "))
-    quantity = int(input("Enter quantity: "))
